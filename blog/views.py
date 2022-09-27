df5c7d77219d1676fe7a45c00595a04e1cf79d8c
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-09-27 20:12:36 +0330

    static and templates initial setup

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ def blog_view(request):
     return render(request, 'blog/blog.html')
 
 def single_view(request):
-    return render(request, 'blog/single.html')
+    return render(request, 'blog/blog-single.html')
