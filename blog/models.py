e2267692151a2bb8281559735074d2fdd60e4b50
ali rafiei <103249712+alirafiei75@users.noreply.github.com>
2022-09-29 18:22:07 +0330

    post and contact models created
    post model created
    contact model created
    admin panel edited

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # image 
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+        
+    def __str__(self):
+        return self.title
+
+
+
